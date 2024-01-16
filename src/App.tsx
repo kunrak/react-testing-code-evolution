d1f6161c4d074cdc10a466f99acffe5450d26c1a
kunrak <rakeshkunducsgi@gmail.com>
2024-01-16 13:37:00 +0530

    till video 47

--- src/App.tsx
+++ src/App.tsx
@@ -7,12 +7,14 @@ import { Skills } from "./components/skills/skills";
 import Counter from "./components/counter/counter";
 import { AppProviders } from "./providers/app-providers";
 import { MuiMode } from "./components/mui/mui-mode";
+import { CounterTwo } from "./components/counter-two/counter-two";
 
 function App() {
   return (
     <AppProviders>
       <div className="App">
         <MuiMode />
+        <CounterTwo count={0} />
       </div>
     </AppProviders>
   );
---
+++ src/mocks/handlers.ts
@@ -0,0 +1,20 @@
+import { http } from 'msw'
+
+export const handlers = [
+    http.get('https://jsonplaceholder.typicode.com/users', (req, res, ctx) => {
+        return res(
+            ctx.status(200),
+            ctx.json([
+                {
+                    name: 'Bruce Wayne',
+                },
+                {
+                    name: 'Clark Kent',
+                },
+                {
+                    name: 'Princess Diana',
+                },
+            ])
+        )
+    }),
+]
